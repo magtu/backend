bfc10e1a782773b8312b7456517fcece61c3efa4
Dmitry Gordin <gordin.mitya@mail.ru>
2016-07-31 21:51:17 +0500

    -call api methods with safety

--- Api/v2/ApiController.php
+++ Api/v2/ApiController.php
@@ -4,12 +4,26 @@ namespace Api\v2;
 
 class ApiController extends \Api\BaseApiController {
     public function process($uri_paths) {
-        echo '2';
+        if (count($uri_paths) == 0) {
+            $this->fail(404);
+        }
+        $methodName = $uri_paths[0].'Method';
+        $callable = array($this, $methodName);
+
+        if (!(method_exists($this, $methodName) && is_callable($callable))) {
+            $this->fail(404);
+        }
+
+        call_user_func($callable, array_slice($uri_paths, 1));
+    }
+    function groupsMethod() {
+
     }
     protected function ok($response) {
         
     }
     protected function fail($code) {
-        
+        echo $code;
+        exit;
     }
 }
